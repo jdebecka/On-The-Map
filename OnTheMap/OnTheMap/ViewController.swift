95401727e7f287c4710e0824fc971f54212cfeb4
Julia Debecka <57728399+jdebecka@users.noreply.github.com>
2020-07-08 20:39:46 +0200

    initial commit

---
+++ OnTheMap/OnTheMap/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OnTheMap
+//
+//  Created by Julia Debecka on 08/07/2020.
+//  Copyright © 2020 Julia Debecka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
